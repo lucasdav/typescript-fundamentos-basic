755def8cad320dfbb82f010500ac53effaf55f80
lucasdav <lucassouzadavanso@gmail.com>
2024-03-24 12:42:03 -0300

    aula 03

---
+++ aula03/genericsV2.ts
@@ -0,0 +1,24 @@
+interface TemNome {
+    nome: string;
+}
+
+class Pessoa2 implements TemNome {
+    nome: string;
+
+    constructor(nome: string) {
+        this.nome = nome;
+    }
+}
+
+class CaixaNome<T extends TemNome> {
+    conteudo: T;
+
+    constructor(conteudo: T) {
+        this.conteudo = conteudo;
+    }
+}
+
+let pessoa4 = new Pessoa2("João");
+let caixa = new CaixaNome(pessoa4);
+
+console.log(caixa.conteudo.nome)
