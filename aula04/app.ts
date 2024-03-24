bfd76ddc6e42814aff119c23540c1b94dac2dcb8
lucasdav <lucassouzadavanso@gmail.com>
2024-03-24 17:41:10 -0300

    aula 04

--- aula04/app.ts
+++ aula04/app.ts
@@ -1,4 +1,6 @@
 import { somar, subtrair } from "./matematica";
+import { MatematicaAvancada } from "./operacoesAvancadas";
 
 console.log(somar(5, 3));
 console.log(subtrair(5,3));
+console.log(MatematicaAvancada.exponenciar(2, 3))
